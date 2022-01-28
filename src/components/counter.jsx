ee87e7da53fd93044a25d819cb05685193d0865d
erfan gharib <erfangharib5@gmail.com>
2022-01-28 15:03:33 +0330

    update: make badge css-class dynamic - set eventHandler on increament button - update state and view by onclick

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     }
 
     render() {
         return (
             <React.Fragment>
-                <h1>{this.state.count}</h1>
-                <button>click me!</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={this.handleIncreament()}
+                    className='py-0.5 px-2 bg-gray-500 text-white rounded-sm hover:bg-gray-700'
+                >
+                    Increment
+                </button>
             </React.Fragment>
         )
+    }
+
+    handleIncreament =()=> {
+        this.setState({ count: this.state.count+1 })
+    }
+    getBadgeClasses() {
+        let classes = 'p-1 font-bold rounded-sm text-stone-900 bg-';
+        classes += this.state.count===0 ? 'yellow-500' : 'blue-500 text-white';
+        return classes;
+    }
+    formatCount() {
+        // object destructuring use for putting object properties
+        // in variable ↓
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
