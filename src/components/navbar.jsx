a044156f915caaad2279d3e45a17117324d2e4ec
erfan gharib <erfangharib5@gmail.com>
2022-02-28 20:57:38 +0330

    show the total number of product

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
-class NavBar extends Component {
-    render() { 
-        return (
-            <nav className='w-full flex items-center px-3 h-16 border-l-8 border-zinc-300 bg-zinc-200 text-zinc-500'>
-                <h2 className='font-bold text-xl'>
-                    NavBar
-                </h2>
-            </nav>
-        );
-    }
+import React from 'react';
+// props used as default arg in state functional component
+const NavBar =props=> {
+    return (
+        <nav className='w-full flex items-center px-3 h-16 border-l-8 border-zinc-300 bg-zinc-200 text-zinc-500'>
+            <h2 className='font-bold text-xl'>
+                Total Product Number :  
+                <span className='ml-3'>
+                    {props.totalCounters}
+                </span>
+            </h2>
+        </nav>
+    );
 }
  
 export default NavBar;
