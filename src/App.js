9b3d2910274c3aab7c8b202f0aab11c1478b57a7
erfan gharib <erfangharib5@gmail.com>
2022-02-28 21:01:48 +0330

    use class for App component - put all app methods in App.js and pass them into child components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import './App.css';
 import './tailwind.css';
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from './components/navbar';
 import Counters from './components/counters';
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className='w-full flex'>
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  }
+
+  handleIncrement = counter => {
+    const counters_ = [...this.state.counters];
+    const index = counters_.indexOf(counter);
+    counters_[index].value++;
+    this.setState({ counters_ });
+  }
+  handleDelete = (counterId) => {
+    // delete a counter
+    const countersFilter = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: countersFilter });
+  }
+  handleReset = () => {
+    // reset all counter and set them to zero
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters })
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c=>c.value>0).length} />
+        <main className='w-full flex'>
+          <Counters
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
